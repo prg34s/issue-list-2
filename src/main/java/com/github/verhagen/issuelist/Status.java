bfd4f9f58f44ffa2ad4a0419aab046e5a170521d
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2015-12-17 19:21:27 +0100

    Add unit tests

--- src/main/java/com/github/verhagen/issuelist/Status.java
+++ src/main/java/com/github/verhagen/issuelist/Status.java
@@ -1,12 +1,18 @@
 package com.github.verhagen.issuelist;
 
+import org.apache.commons.lang3.StringUtils;
+
 public enum Status {
+	CLOSED,
 	OPEN;
 
 	public static Status fromText(String name) {
-		if (name == null) {
+		final String nameCln = StringUtils.trimToNull(name);
+		if (nameCln == null) {
 			throw new IllegalArgumentException("Argument 'name' should not be null.");
 		}
+
+		Status[] values = Status.values();
 		return OPEN;
 	}
 
--- src/test/java/com/github/verhagen/issuelist/StatusTest.java
+++ src/test/java/com/github/verhagen/issuelist/StatusTest.java
@@ -16,12 +16,44 @@ public class StatusTest {
 	}
 
 	@Test
+	public void createClosed() throws Exception {
+		String statusStr = "closed";
+		Status status = Status.fromText(statusStr);
+		
+		assertNotNull(status);
+		assertEquals(statusStr.toUpperCase(), status.name());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void createHelp() throws Exception {
+		try {
+			Status.fromText("help");
+		}
+		catch (IllegalArgumentException iae) {
+			assertEquals("Argument 'name' with value 'help' is not a known Status.", iae.getMessage());
+			throw iae;
+		}
+	}
+
+	@Test(expected = IllegalArgumentException.class)
 	public void createNull() throws Exception {
 		try {
 			Status.fromText(null);
 		}
 		catch (IllegalArgumentException iae) {
 			assertEquals("Argument 'name' should not be null.", iae.getMessage());
+			throw iae;
+		}
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void createSpaces() throws Exception {
+		try {
+			Status.fromText("   ");
+		}
+		catch (IllegalArgumentException iae) {
+			assertEquals("Argument 'name' should not be null.", iae.getMessage());
+			throw iae;
 		}
 	}
 
